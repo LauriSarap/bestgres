50f8c6a1b5cbc98ff95acec97238f5028d6aa158
Lauri Sarap <lauri.aardla@gmail.com>
2026-02-15 02:40:40 +0100

    icon fix

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,12 +3,21 @@ mod db;
 mod models;
 
 use commands::connection::AppState;
+use tauri::Manager;
+
+const APP_ICON: tauri::image::Image<'_> = tauri::include_image!("icons/icon.png");
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .manage(AppState::new())
+        .setup(|app| {
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.set_icon(APP_ICON.clone());
+            }
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![
             commands::connection::add_connection,
             commands::connection::update_connection,
